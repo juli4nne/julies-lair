5d8834c32e2ca1a01109276951df9ea07784ef84
juli4nne <tico.lobo03@gmail.com>
2026-02-28 14:36:34 -0500

    add frieren theme + minor other theme changes

--- scripts/code.js
+++ scripts/code.js
@@ -21,7 +21,24 @@ function toggleDropdown(id) {
     }
 }
 
+function randomNekoStamp() {
+    const image = document.getElementById("nekowebStamp");
 
+    chosen = Math.floor(Math.random() * 10) + 1;
+    switch (chosen) {
+        case 1: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button1.gif"); break;
+        case 2: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button2.gif"); break;
+        case 3: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button3.gif"); break;
+        case 4: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button4.gif"); break;
+        case 5: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button5.gif"); break;
+        case 6: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button6.gif"); break;
+        case 7: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button7.gif"); break;
+        case 8: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button8.gif"); break;
+        case 9: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button9.gif"); break;
+        case 10: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button10.png"); break;
+        case 11: image.setAttribute("src", "https://nekoweb.org/assets/buttons/button11.gif"); break;
+    }
+}
 
 function swapDarkMode() {
     const theme = document.getElementById("theme");
@@ -33,8 +50,10 @@ function swapLightMode() {
     theme.setAttribute("href", "./css/themes/lightmode.css");
     const width = element.offsetWidth;
 }
-/* function swapFrierenTheme() {
+function swapFrierenTheme() {
     const theme = document.getElementById("theme");
-    theme.setAttribute("href", "/css/themes/.css");
+    theme.setAttribute("href", "/css/themes/frieren.css");
     const width = element.offsetWidth;
-} */
+} 
+
+window.onload = randomNekoStamp;
